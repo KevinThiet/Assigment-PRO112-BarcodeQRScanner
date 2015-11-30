d0d8de269a4036a972f22a9a55db6ff79905073d
ThietBlue <kevinthiet9696@gmail.com>
2015-11-30 23:32:38 +0700

    Vox Đình Thiết - Commit 2

---
+++ src/com/assigment/barcode/history/HistoryItemAdapter.java
@@ -0,0 +1,59 @@
+
+package com.assigment.barcode.history;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.zxing.Result;
+import com.google.zxing.client.android.R;
+
+final class HistoryItemAdapter extends ArrayAdapter<HistoryItem> {
+
+	private final Context activity;
+
+	HistoryItemAdapter(Context activity) {
+		super(activity, R.layout.history_list_item, new ArrayList<HistoryItem>());
+		this.activity = activity;
+	}
+
+	@Override
+	public View getView(int position, View view, ViewGroup viewGroup) {
+		View layout;
+		if (view instanceof LinearLayout) {
+			layout = view;
+		} else {
+			LayoutInflater factory = LayoutInflater.from(activity);
+			layout = factory.inflate(R.layout.history_list_item, viewGroup, false);
+		}
+
+		HistoryItem item = getItem(position);
+		Result result = item.getResult();
+
+		CharSequence title;
+		CharSequence detail;
+		if (result != null) {
+			title = result.getText();
+			detail = result.getText();
+		} else {
+			Resources resources = getContext().getResources();
+			title = resources.getString(R.string.history_empty);
+			detail = resources.getString(R.string.history_empty_detail);
+		}
+		
+		((TextView) layout.findViewById(R.id.history_title)).setText(title);
+		((TextView) layout.findViewById(R.id.history_detail)).setText(detail);
+         
+		return layout;
+	}
+
+}
